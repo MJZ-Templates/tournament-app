3e522fe14f639b93ec378aa849538517d91a6a01
misung-dev <misung.dev@gmail.com>
2025-02-28 15:35:59 +0900

    style: 타이틀 영역만큼 차지

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,28 @@
-// src/components/Header.jsx
-import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const HeaderContainer = styled.header`
+  padding: 0;
+`;
+
+const Title = styled.h1`
+  padding: 20px;
+  width: fit-content;
+  cursor: pointer;
+  transition: color 0.3s ease-in-out;
+`;
 
 const Header = ({ tournamentTitle, pageTitle }) => {
+  const navigate = useNavigate();
+
   return (
-    <header>
-      <h1>
+    <HeaderContainer>
+      <Title onClick={() => navigate("/")}>
         {tournamentTitle
           ? `${tournamentTitle} - ${pageTitle}`
           : "Tournament App"}
-      </h1>
-    </header>
+      </Title>
+    </HeaderContainer>
   );
 };
 
