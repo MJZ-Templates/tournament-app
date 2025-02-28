871b9d178996114d5708b090551eb3dbff1e8ff9
misung-dev <misung.dev@gmail.com>
2025-02-28 13:27:57 +0900

    feat: 이미지를 크게 보여주고 선택하는 기능 구현

--- src/components/Match.jsx
+++ src/components/Match.jsx
@@ -1,17 +1,41 @@
 import React from "react";
+import styled from "styled-components";
+
+const MatchContainer = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const Player = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  transition: transform 0.2s ease-in-out;
+
+  &:hover {
+    transform: scale(1.05);
+  }
+`;
+
+const PlayerImage = styled.img`
+  max-width: 90%;
+  max-height: 90%;
+  object-fit: contain;
+`;
 
 const Match = ({ player1, player2, onSelectWinner }) => {
   return (
-    <div className="match">
-      <div className="player">
-        <img src={player1} alt="Player 1" />
-        <button onClick={() => onSelectWinner(player1)}>Select</button>
-      </div>
-      <div className="player">
-        <img src={player2} alt="Player 2" />
-        <button onClick={() => onSelectWinner(player2)}>Select</button>
-      </div>
-    </div>
+    <MatchContainer>
+      <Player onClick={() => onSelectWinner(player1)}>
+        <PlayerImage src={player1} alt="Player 1" />
+      </Player>
+      <Player onClick={() => onSelectWinner(player2)}>
+        <PlayerImage src={player2} alt="Player 2" />
+      </Player>
+    </MatchContainer>
   );
 };
 
